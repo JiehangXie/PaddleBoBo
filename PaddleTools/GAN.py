914a613a9027cc213a7d923d5476b902d529c589
ldh8888 <p2374473@gmail.com>
2023-03-12 13:15:26 +0800

    Update GAN.py

--- PaddleTools/GAN.py
+++ PaddleTools/GAN.py
@@ -5,7 +5,7 @@ from ppgan.apps.first_order_predictor import FirstOrderPredictor
 #热加载
 wav2lip_predictor = Wav2LipPredictor(face_det_batch_size = 2,
                                  wav2lip_batch_size = 16,
-                                 face_enhancement = True)
+                                 face_enhancement = False)
 
 
 def wav2lip(input_video,input_audio,output):
